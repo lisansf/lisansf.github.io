77e2c99dcf1055940c773b9f8bdb9d170c0cee81
lisansf <lisanshidqifarizan@gmail.com>
2024-08-06 11:13:14 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+
 import './App.css';
+
+import Home from "./components/Home"
+import Blog from "./components/Blog"
+import Store from "./components/Store"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <nav>
+            <p>Lisan</p>
+            <div>
+              <Link to="/">Home</Link>
+              <Link to="/Blog">Blog</Link>
+              <Link to="/Store">Store</Link>
+            </div>
+          </nav>
+        </header>
+        <main>
+          <Routes>
+            <Route exact path="/" element={<Home />}/>
+            <Route path="/Blog" element={<Blog />}/>
+            <Route path="/Store" element={<Store />}/>
+          </Routes>
+        </main>
+        <footer>
+          <div>
+              <Link to="/">Home</Link>
+              <Link to="/Blog">Blog</Link>
+              <Link to="/Store">Store</Link>
+          </div>
+        </footer>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Store.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Store = () => {
+    return(
+        <>
+            <div>
+                <p>Store</p>
+            </div>
+        </>
+    )
+}
+
+export default Store;
