f8f03fc3cb68598b25ac3781a985ec78092ec28b
lisansf <lisanshidqifarizan@gmail.com>
2026-05-06 10:09:27 +0700

    fix meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,8 @@ import { M_PLUS_Rounded_1c } from "next/font/google";
 import "./globals.css";
 
 export const metadata: Metadata = {
-	title: "Lisan Shidqi Farizan - Web Developer",
-	description: "Portfolio Lisan Shidqi Farizan, mahasiswa Informatika dan Fullstack Developer yang fokus pada pengembangan web modern menggunakan Next.js, React, TypeScript, dan MongoDB. Berpengalaman membangun aplikasi full-stack serta tertarik pada pengembangan backend, dan analisis sistem.",
-	icons: {
-		icon: "/img/favicon.png",
-	},
+	title: "Lisan Shidqi Farizan | Frontend & Fullstack Developer (Next.js, React, TypeScript)",
+	description: "Portfolio Lisan Shidqi Farizan, Frontend & Fullstack Developer yang fokus pada pengembangan web modern menggunakan Next.js, React, TypeScript, dan MongoDB.",
 	keywords: [
 		"Lisan Shidqi Farizan",
 		"Frontend Developer",
@@ -18,10 +15,35 @@ export const metadata: Metadata = {
 		"TypeScript",
 		"MongoDB",
 		"Web Developer Indonesia"
-	]
+	],
 	authors: [
 		{ name: "Lisan Shidqi Farizan", url: "https://veoveneht.eu.org/" },
 	],
+	icons: {
+		icon: "/img/favicon.png",
+	},
+	openGraph: {
+		title: "Lisan Shidqi Farizan | Web Developer",
+		description: "Portfolio Lisan Shidqi Farizan, Frontend & Fullstack Developer.",
+		url: "https://veoveneht.eu.org/",
+		siteName: "Lisan Portfolio",
+		images: [
+		{
+			url: "/img/lisan.png",
+			width: 1200,
+			height: 630,
+		},
+		],
+		locale: "id_ID",
+		type: "website",
+	},
+	robots: {
+		index: true,
+		follow: true,
+	},
+	alternates: {
+	canonical: "https://lisansf.vercel.app",
+	}
 };
 
 const mplus = M_PLUS_Rounded_1c({
@@ -65,6 +87,23 @@ export default function RootLayout({
 					crossOrigin="anonymous"
 					strategy="afterInteractive"
 				></Script>
+				<Script
+					id="ld-json"
+					type="application/ld+json"
+					dangerouslySetInnerHTML={{
+						__html: JSON.stringify({
+						"@context": "https://schema.org",
+						"@type": "Person",
+						name: "Lisan Shidqi Farizan",
+						url: "https://veoveneht.eu.org/",
+						sameAs: [
+							"https://github.com/username",
+							"https://linkedin.com/in/username"
+						],
+						jobTitle: "Frontend Developer",
+						}),
+					}}
+				/>
 			</body>
 		</html>
 	);
